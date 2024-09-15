274b1a22cbfaa121e85a7cad69486af2c41dcf2e
l241025097 <luo241025097@sina.com>
2024-09-15 11:39:57 +0800

    修改测试socket服务端程序bug

--- src/test_socket_server.py
+++ src/test_socket_server.py
@@ -1,9 +1,12 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(("127.0.0.1", 12345))
+ipv4=socket.gethostbyname_ex(socket.gethostname())[2][0]
+sock.connect((ipv4, 12345))
 
 send_msg = b"test massage"
-sock.send(send_msg)
+sock.send(send_msg, )
 print(send_msg.decode())
+receive_msg = sock.recv(1024).decode()
+print(receive_msg)
 sock.close()
