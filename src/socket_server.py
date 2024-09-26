86af4134f7faec98d5866a7150b9831abd722695
luoyl25 <luoyl25@chinaunicom.cn>
2024-09-26 17:52:15 +0800

    修改为多进程

--- src/socket_server.py
+++ src/socket_server.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import threading
 from utils import get_log, connect_mq
 from datetime import datetime
 
@@ -14,44 +15,46 @@ def create_server(port=12345):
     server_socket.listen(5)
     return server_socket
 
+def tcplink(client_socket, addr, log_obj):
+    received_message = b""
+    while True:
+        data = client_socket.recv(1)
+        if not data:
+            break
+        received_message += data
+        try:
+            received_message_str = received_message.decode('utf-8')
+        except:
+            pass
+    connection, channel = connect_mq()
+    try:
+        channel.basic_publish(
+            exchange='', # 当前是一个简单模式，所以这里设置为空字符串就可以了
+            routing_key='sms', # 指定消息要发送到哪个queue
+            body=received_message # 指定要发送的消息,
+        )
+    except Exception as err:
+        log_obj.exception(err)
+    finally:
+        connection.close()
+    # 打印从客户端接收到的消息
+    log_obj.info(f"Received message from {addr}: {received_message_str}")
+
+    # 发送一个响应消息给客户端
+    response_message = "Hello, Client! I received your message.".encode('utf-8')
+    client_socket.send(response_message)
+
+    # 关闭与客户端的连接
+    client_socket.close()
+
 def execute(socket_server, log_obj):
 
     while True:
-        # 接受一个新的连接
-        client_socket, _ = socket_server.accept()
-        
-        # 接收客户端发送的数据
-        received_message = b""
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            received_message += data
-            try:
-                received_message_str = received_message.decode('utf-8')
-            except Exception as err:
-                log_obj.info(err)
-        connection, channel = connect_mq()
-        try:
-            channel.basic_publish(
-                exchange='', # 当前是一个简单模式，所以这里设置为空字符串就可以了
-                routing_key='sms', # 指定消息要发送到哪个queue
-                body=received_message # 指定要发送的消息,
-            )
-        except Exception as err:
-            log_obj.exception(err)
-        finally:
-            connection.close()
-        # 打印从客户端接收到的消息
-        log_obj.info("Received message: " + received_message_str)
-        
-        # # 发送一个响应消息给客户端
-        # response_message = "Hello, Client! I received your message.".encode('utf-8')
-        # client_socket.send(response_message)
-        
-        # # 关闭与客户端的连接
-        # client_socket.close()
-
+        # 接受一个新连接:
+        client_socket, addr = socket_server.accept()
+        # 创建新线程来处理TCP连接:
+        t = threading.Thread(target=tcplink, args=(client_socket, addr, log_obj))
+        t.start()
 
 if __name__ == "__main__":
     BEGIN = datetime.now()
